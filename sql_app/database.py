08a952985fd1321ab47b4b78b3ba3e23c0f58df4
tiago.blima <tiago.blima02@gmail.com>
2021-10-26 20:33:00 -0300

    adding dot-env requirements and .env variable

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,9 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
 
-#os.environ['DATABASE_URI']
-SQLALCHEMY_DATABASE_URL = "postgresql://retaqaobekgobi:242f10845a36fc9d3b20fe12bd848612211af5071a46985b1cea5d09756c2304@ec2-52-87-123-108.compute-1.amazonaws.com:5432/dc8ov60mfu4nu7"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URI')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
