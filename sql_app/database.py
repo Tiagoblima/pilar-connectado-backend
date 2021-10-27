c2e75aec75dd88d444164c69ca124a9beee9cd82
tiago.blima <tiago.blima02@gmail.com>
2021-10-26 22:01:52 -0300

    adding user crud

--- sql_app/database.py
+++ sql_app/database.py
@@ -6,11 +6,9 @@ from dotenv import load_dotenv
 load_dotenv('sql_app/.env')
 
 SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URI')
-#os.getenv('DATABASE_URI')
-print(os.getenv('DATABASE_URI'))
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args={}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
