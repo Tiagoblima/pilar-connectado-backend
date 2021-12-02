cb9bf5f88135fee4f8eb4a7f049cb9ae5c387f7c
Tiago de Lima <tiago.blima@ufrpe.br>
2021-12-02 20:58:59 -0300

    There is no porto member with this id user.

--- main.py
+++ main.py
@@ -183,7 +183,10 @@ def read_pilar_member(skip: int = 0, limit: int = 100, db: Session = Depends(get
 def read_pilar_member(user_id: int, db: Session = Depends(get_db)):
     pilar_mbm = crud.get_pilar_member_by_user_id(db, user_id)
     if not pilar_mbm:
-        raise HTTPException(status_code=404, detail="There is no pilar member with this id user.")
+        return {
+            "success": False,
+            "detail": "There is no pilar member with this id user."
+        }
     return jsonable_encoder(pilar_mbm)
 
 
