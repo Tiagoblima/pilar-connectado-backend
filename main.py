363d2367aa895d480b27f13e2d508b89fd81e218
Tiago de Lima <tiago.blima@ufrpe.br>
2021-12-02 21:18:08 -0300

    There is no porto member with this id user.

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ def read_pilar_member(skip: int = 0, limit: int = 100, db: Session = Depends(get
     return jsonable_encoder(pilar_mbm)
 
 
-@app.get("/v1/pilar_member/by/user/{user_id}/", response_model=schemas.SchemePilarMember, tags=["Pilar Member"])
+@app.get("/v1/pilar_member/by/user/{user_id}/", tags=["Pilar Member"])
 def read_pilar_member(user_id: int, db: Session = Depends(get_db)):
     pilar_mbm = crud.get_pilar_member_by_user_id(db, user_id)
     if not pilar_mbm:
